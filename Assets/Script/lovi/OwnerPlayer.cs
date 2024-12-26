18a499f4f58ca8d17c2ce3c3a71bfddea61a08e9
WIN-1Q8DR817JKE\user <aklius14pg@gmail.com>
2024-12-27 03:11:31 +0900

    にょん

---
+++ Assets/Script/lovi/OwnerPlayer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwnerPlayer : MonoBehaviour
+{
+    public float speed = 4f;
+
+    private Rigidbody2D rb;
+    private Vector3 nextPos;
+    private Vector3 direction;
+    private bool isMoving = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (isMoving) MoveToNextPos();
+    }
+    public void ClickMove(Vector3 worldPosition)
+    {
+        nextPos = worldPosition;
+        isMoving = true;
+    }
+
+    void MoveToNextPos()
+    {
+        Vector3 direction = (nextPos - transform.position).normalized;
+        float step = speed * Time.fixedDeltaTime;
+        rb.MovePosition(transform.position + direction * step);
+
+        if (Vector3.Distance(transform.position, nextPos) < 0.1f)
+        {
+            transform.position = nextPos; // 最終的に位置を正確に合わせる
+            isMoving = false;
+        }
+    }
+
+}
